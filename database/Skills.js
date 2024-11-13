0552fb48d7f6fbfe1ef094d4a619f74b66781748
AshmiyaV <ashmiyavs6767@gmail.com>
2024-11-13 14:49:10 -0500

    skills

---
+++ database/Skills.js
@@ -0,0 +1,148 @@
+import { DiMongodb, DiPostgresql, DiMysql } from "react-icons/di";
+import { SiReact, SiSpringboot, SiBootstrap, SiExpress, 
+  SiRedux, SiGradle, SiApachejmeter, SiHibernate, SiTerraform, 
+  SiGooglecloud, SiMicrosoftazure, SiIbmcloud, SiOracle, 
+  SiNumpy, SiPandas } from "react-icons/si";
+
+export const Skills = [
+  {
+    "icon": "IconJS",
+    "image":  "/icon/javascript.svg",
+    "title": "JavaScript",
+    "subtitle": "Vanilla, jQuery",
+    "description": "I bring websites to life with JavaScript, adding interactive features and optimizing performance with clean and efficient code."
+  },
+  {
+    "icon": "IconOptimization",
+    "image":  "/icon/typescript.svg",
+    "title": "TypeScript",
+    "subtitle": "Insights",
+    "description": "I specialize in enhancing website performance, ensuring fast loading times and efficient resource utilization for an optimal user experience."
+  },
+  {
+    "icon": "IconReact",
+    "image": "/icon/react.svg",
+    "title": "React JS",
+    "subtitle": "Next.js, Vercel",
+    "description": "Using React, I craft scalable user interfaces with components, state management, and virtual DOM manipulation for seamless web applications."
+  },
+  {
+    "icon": "IconA11Y",
+    "image": "/icon/redux.svg",
+    "title": "Redux",
+    "subtitle": "Web Accessibility",
+    "description": "Accessibility is a must. I ensure everyone can use what I create, focusing on keyboard navigation and screen reader compatibility."
+  },
+  {
+    "icon": "IconCSS",
+    "image": "/icon/css.svg",
+    "title": "CSS",
+    "subtitle": "Animations, Sass",
+    "description": "I prioritize pixel perfection, paying close attention to details for visually polished and precisely aligned designs, ensuring a seamless user experience."
+  },
+  {
+    "icon": "IconHTML",
+    "image": "/icon/html.svg",
+    "title": "HTML",
+    "subtitle": "Semantic Markup",
+    "description": "Beyond markup, HTML is crucial for an inclusive web. I structure content carefully for universal accessibility."
+  },
+  {
+    "icon": "IconA11Y",
+    "image": "/icon/sass.svg",
+    "title": "Sass",
+    "subtitle": "Web Accessibility",
+    "description": "Accessibility is a must. I ensure everyone can use what I create, focusing on keyboard navigation and screen reader compatibility."
+  },
+  {
+    "icon": "IconFigma",
+    "image": "/icon/figma.svg",
+    "title": "Design",
+    "subtitle": "Figma & Photoshop",
+    "description": "While not a full-time UI designer, I have an eye for aesthetics, creating visually appealing and user-friendly interfaces."
+  },
+  {
+    "icon": "IconAPI",
+    "image": "/icon/rest.svg",
+    "title": "REST APIs",
+    "subtitle": "Axios x Fetch",
+    "description": "I integrate data and functionality seamlessly with REST APIs, creating dynamic web applications for enhanced performance."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/java.svg",
+    "title": "Java",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/springboot.svg",
+    "title": "SpringBoot",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/nodejs.svg",
+    "title": "Node.js",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/expressjs.svg",
+    "title": "Express.js",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/python.svg",
+    "title": "Python",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/mongodb.svg",
+    "title": "MongoDB",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/mysql.svg",
+    "title": "MySQL",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/postgresql.svg",
+    "title": "PostgreSQL",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/google_cloud.svg",
+    "title": "Google Cloud Provider",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/aws.svg",
+    "title": "Amazon Web Services",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+  {
+    "icon": "IconGSAP",
+    "image": "/icon/terraform.svg",
+    "title": "Terraform",
+    "subtitle": "Scroll Animations",
+    "description": "I use GSAP for captivating animations that bring web pages to life, enhancing the overall user experience."
+  },
+]
